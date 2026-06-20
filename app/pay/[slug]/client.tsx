18d357fa832dffd4fca4b22ae6f6e07628566ec9
Abbalolo <verifiedaboki@gmail.com.com>
2026-06-20 19:38:38 +0100

    pyment page minor fix

--- app/pay/[slug]/client.tsx
+++ app/pay/[slug]/client.tsx
@@ -967,7 +967,7 @@ export default function PaymentPageClient({ slug }: PaymentPageClientProps) {
       return;
     }
 
-    const transferReference = `TRF-${Date.now()}-${Math.random().toString(36).substring(2, 6)}`;
+    const transferReference = `PPL-${Date.now()}-${Math.random().toString(36).substring(2, 6)}`;
     setPendingReference(transferReference);
     const narration = generateNarrationCode();
     setNarrationCode(narration);
