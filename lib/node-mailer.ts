c3e808348dfc4fcbd6f67d559fe63ffc3fe6cfad
Abbalolo <verifiedaboki@gmail.com.com>
2026-07-22 13:23:07 +0100

    error

--- lib/node-mailer.ts
+++ lib/node-mailer.ts
@@ -1,23 +1,23 @@
 import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  
 // export const transporter = nodemailer.createTransport({
 //   host: "smtp.gmail.com",
-//     port: 465,
-//   secure: true,
+//     port: 587,
+//     secure: false,
+//     requireTLS: true,
 //   auth: {
 //     user: process.env.EMAIL_USER,
 //     pass: process.env.EMAIL_PASS,
 //   },
+  
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+    port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 
